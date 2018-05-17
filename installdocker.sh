b755a9ff7da6790b8ba7ba7a266f945e1c8562e2
Josh Hu <josh.hu@yahoo.com>
2018-05-17 17:37:13 +0800

    add docker-compose

--- installdocker.sh
+++ installdocker.sh
@@ -1,3 +1,5 @@
 curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker vagrant
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
