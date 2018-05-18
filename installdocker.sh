e5388f2d4705cc46efd46bd76bb9a027229fb7d3
Josh Hu <josh.hu@yahoo.com>
2018-05-18 11:44:45 +0800

    installdocker

--- installdocker.sh
+++ installdocker.sh
@@ -1,6 +1,8 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
 curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo usermod -aG docker vagrant
+sudo usermod -aG docker $USER
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker run -v /usr/local/bin:/target jpetazzo/nsenter
